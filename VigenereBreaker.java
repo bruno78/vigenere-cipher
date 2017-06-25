97d44c1c61d157898fbcb0c30f6b97f4657cefce
bruno78 <devbruno78@gmail.com>
2017-06-25 17:30:14 -0400

    Add sliceString method and test

--- VigenereBreaker.java
+++ VigenereBreaker.java
@@ -3,8 +3,11 @@ import edu.duke.*;
 
 public class VigenereBreaker {
     public String sliceString(String message, int whichSlice, int totalSlices) {
-        //REPLACE WITH YOUR CODE
-        return "WRITE ME!";
+        StringBuilder slicedString = new StringBuilder("");
+        for (int k = whichSlice; k < message.length(); k += totalSlices) {
+            slicedString.append(message.substring(k, k+1));
+        }
+        return slicedString.toString();
     }
 
     public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
---
+++ tester.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class tester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class tester
+{
+    public void testSliceString(){
+        VigenereBreaker vb = new VigenereBreaker();
+        System.out.println(vb.sliceString("abcdefghijklm", 0, 3));
+        System.out.println(vb.sliceString("abcdefghijklm", 1, 3));
+        System.out.println(vb.sliceString("abcdefghijklm", 2, 3));
+        System.out.println(vb.sliceString("abcdefghijklm", 0, 4));
+        System.out.println(vb.sliceString("abcdefghijklm", 1, 4));
+        System.out.println(vb.sliceString("abcdefghijklm", 2, 4));
+        System.out.println(vb.sliceString("abcdefghijklm", 3, 4));
+        System.out.println(vb.sliceString("abcdefghijklm", 0, 5));
+        System.out.println(vb.sliceString("abcdefghijklm", 1, 5));
+        System.out.println(vb.sliceString("abcdefghijklm", 2, 5));
+        System.out.println(vb.sliceString("abcdefghijklm", 3, 5));
+        System.out.println(vb.sliceString("abcdefghijklm", 4, 5));
+    }
+}
